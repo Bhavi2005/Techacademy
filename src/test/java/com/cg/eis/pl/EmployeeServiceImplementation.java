fc943fc1d858b29ca5d6e5ab28d5da3024ab0126
Bhavitha <kamathambhavitha2003@gmail.com>
2025-08-07 12:22:54 +0530

    First Commit

---
+++ src/test/java/com/cg/eis/pl/EmployeeServiceImplementation.java
@@ -0,0 +1,36 @@
+package com.cg.eis.pl;
+
+import com.cg.eis.bean.Employee;
+import com.cg.eis.service.EmployeeService;
+
+public class EmployeeServiceImplementation implements EmployeeService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String setInsuranceScheme(Employee emp) {
+		// TODO Auto-generated method stub
+		
+		if(emp.getDesignation() =="Manager") {
+			return "A+";
+		}
+		else if(emp.getDesignation() == "Developer")
+		{
+			return "B+";
+		}
+		else if(emp.getDesignation() == "Tester") {
+			return "A++";
+		}
+		else {
+			{
+				return "O+";
+			}
+		}
+		
+	}
+
+
+}
